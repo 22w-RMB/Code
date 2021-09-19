a05b461f4b7bcd406ac132f59cd5379ed037795a
22w-RMB <2826067275@qq.com>
2021-09-19 21:45:16 +0800

    增加 新增项目接口测试用例

--- PythonCode/InterfaceTest/test_cases/test_10_add_loan.py
+++ PythonCode/InterfaceTest/test_cases/test_10_add_loan.py
@@ -1,12 +1,11 @@
-
-
+import json
 import unittest
 
 from common import my_ddt
 from common.excel_handler import ExcelHandler
-from common.python_handler import ExcelConfig
+from common.python_handler import ExcelConfig, OtherConfig
 from common.request_handler import RequestHandler
-from precondition.helper import Context
+from precondition.helper import Context, replaceLabel
 
 
 @my_ddt.ddt
@@ -28,7 +27,29 @@ class TestAddLoan(unittest.TestCase):
     @my_ddt.data(*testData)
     def test_add_laon(self, singleData):
 
+        url = OtherConfig.remoteHost + singleData['url']
+
+        headers = json.loads(singleData['headers'])
+        headers['Authorization'] = self.context.token
+
+        requestData = replaceLabel(singleData['data'])
+
+        if "$wrong_member_id$" in requestData:
+            requestData = requestData.replace("$wrong_member_id$", str(self.context.member_id + 10))
+
+        responseData = self.session.visit(method=singleData['method'],
+                                          url=url,
+                                          headers=headers,
+                                          json = json.loads(requestData))
+
+        print(responseData)
+
+        try:
+            self.assertEqual(singleData['expect'], responseData['code'])
+            print("测试用例通过")
+        except AssertionError as e:
+            print("测试用例不通过： %s " % e)
+            raise e
 
 
-        pass
 
